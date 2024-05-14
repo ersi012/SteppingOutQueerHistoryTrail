38fcff2cefe139881c6924736cdec70eb08bdde9
ersi <ersildacako14@gmail.com>
2024-05-13 23:25:32 -0400

    FirstParishChurch page updates

--- src/app/FirstParishChurch/page.js
+++ src/app/FirstParishChurch/page.js
@@ -1,10 +1,19 @@
+import Image from 'next/image';
 
-
-export default function FirstParishChurch(){
+const FirstParishChurch = () => {
     return (
-        <main>
-            <h1>First</h1>
+      <div>
+        <img
+          src="/images/sites/FirstParish1.jpg"
+          alt="Description of your image"
+          width={800} // Set your preferred width
+          height={600} // Set your preferred height
+        />
         
-        </main>
-    )
-}
+        <h1>First Parish Church</h1>
+        <p>This is your homepage content...</p>
+      </div>
+    );
+  };
+  
+  export default  FirstParishChurch;
