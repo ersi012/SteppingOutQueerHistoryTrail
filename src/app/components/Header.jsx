45b45ee79bb8cc0f3f4f1caa7382145853f71d4f
ersi <ersildacako14@gmail.com>
2024-06-10 21:55:55 -0400

    test

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,8 +7,7 @@ const Header = () => {
     <section className='bg-[#1C5C2D] w-screen min-h-screen px-20 py-20 flex justify-center items-center'>
         <div className='grid grid-cols-1 lg:grid-cols-12'>
             <div className='col-span-8 place-self-center text-center sm:text-left '>
-                <h1 className='text-[#E2D6AF] font-serif text-7xl font-semibold mb-9 ml-8'>Stepping Out: the Portland, Maine Queer History Trail
-</h1>
+                <h1 className='text-[#E2D6AF] font-serif text-7xl font-semibold mb-9 ml-8'>Stepping Out: the Portland, Maine Queer History Trail</h1>
                 <p className='text-[#C7BEA0] font-serif text-xl md:text-left mb-7 ml-8'>This 1.5 mile self-guided tour will take you through the downtown of Maine’s largest city along Congress and Pine Streets in Portland. Along the way, you will explore locations and events important to local LGBTQ+ history. 
                 </p>
                 <audio controls>
@@ -17,15 +16,15 @@ const Header = () => {
             </div>
 
             <div className='col-span-4 place-self-center lg:m-0 mt-4'>
-            <div className='rounded-full bg-[#E2D6AF] w-[400px] h-[400px] relative overflow-hidden flex items-center justify-center'>
-                <div className='rounded-full w-[350px] h-[350px] overflow-hidden'>
-                <Image
-                    src='./images/header.webp'
-                    className="w-full h-full rounded-full"
-                    alt="Two people sitting in a car reaching back holding on to a rainbow flag"
-                    width={350}
-                    height={300}
-                />
+  <div className='rounded-full bg-[#E2D6AF] w-full max-w-[400px] aspect-square relative overflow-hidden flex items-center justify-center'>
+    <div className='rounded-full w-11/12 max-w-[350px] aspect-square overflow-hidden'>
+      <Image
+        src='./images/header.webp'
+        className="w-full h-full object-cover rounded-full"
+        width={350}
+        height={300}
+        alt="Header Image"
+      />
                 </div>
             </div>
             </div>
